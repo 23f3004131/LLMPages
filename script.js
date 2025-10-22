876658602133ce809ac054b35dfd480ffebafee6
Nitin Yadav <155138107+23f3004131@users.noreply.github.com>
2025-10-22 13:58:14 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+{
+    "people": 3,
+    "case_1": {
+        "swerve": false,
+        "reason": "Avoiding greater harm; hitting 1 person is less harmful than 2."
+    },
+    "case_2": {
+        "swerve": true,
+        "reason": "The single person is a child, prioritizing their safety over the criminals."
+    }
+}
